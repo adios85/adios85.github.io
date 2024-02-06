cbeed489d31bbd15c5d33da0b87f12ce791e665c
adios85 <96771971+adios85@users.noreply.github.com>
2024-02-06 17:58:05 +0300

    Update custom2.js

--- custom2.js
+++ custom2.js
@@ -4,6 +4,8 @@
 function listenMykeys(e) {
   if (e.keyCode === 97 || e.keyCode === 49) {
 	Lampa.Search.open();
+  }
+}
 
 $(document).ready(function startSmartHome() {
 	document.addEventListener("keydown", listenMykeys);
