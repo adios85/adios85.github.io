c88bea28c3cc1642beafc4b6e2ee800ab952d9e7
adios85 <96771971+adios85@users.noreply.github.com>
2024-02-06 19:55:30 +0300

    Update search.js

--- search.js
+++ search.js
@@ -2,11 +2,8 @@
     'use strict';
 
 function listenMykeys(e) {
-  if (e.keyCode === 97 || e.keyCode === 49) {
-	Lampa.Search.open();
-	setInterval(function(){
-		Lampa.Utils.trigger(document.querySelector(".simple-keyboard-mic"), 'click');
-	},1500)
+  if (e.keyCode === 184 || e.keyCode === 184) {
+	Lampa.Search.open()
   }
 }
 
