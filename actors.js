a2d252c825b2b6d1130ba6aa412ac8c66cc75465
adios85 <96771971+adios85@users.noreply.github.com>
2026-03-18 17:20:48 +0300

    Update actors.js

--- actors.js
+++ actors.js
@@ -1,77 +1,225 @@
-// TMDB Actor Cards Fix for Lampa
+(function () {
+    'use strict';
 
-var ActorModule = {
+    var PLUGIN_NAME = 'persons_plugin';
+    var STORAGE_KEY = 'persons_favorites';
+    var PAGE_SIZE = 20;
+    var currentId = null;
 
-// Получение данных актёра из TMDB
-getActor: function(actorId, callback) {
-    var url = 'https://tmdb-api.rootu.top/3/person/' + actorId + '?api_key=YOUR_API_KEY&language=ru';
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data && data.id) {
-                var card = {
-                    id: data.id,
-                    title: data.name,
-                    name: data.name,
-                    poster_path: data.profile_path,
-                    profile_path: data.profile_path,
-                    type: 'person',
-                    card_type: 'person',
-                    component: 'person',   // строго person
-                    method: 'person',
-                    source: 'tmdb',
-                    media_type: 'person',
-                    url: 'person/' + data.id
-                };
-                callback(card);
-            } else {
-                console.warn('Actor not found:', actorId);
-            }
-        })
-        .catch(err => console.error('TMDB Actor fetch error:', err));
-},
-
-// Создание карточки на странице
-createCard: function(card) {
-    var container = document.querySelector('#actor-cards');
-    if (!container) {
-        container = document.createElement('div');
-        container.id = 'actor-cards';
-        document.body.appendChild(container);
+    function log() {
+        console.log.apply(console, arguments);
     }
 
-    var cardEl = document.createElement('div');
-    cardEl.className = 'actor-card';
-    cardEl.innerHTML = `
-        <img src='https://image.tmdb.org/t/p/w300${card.poster_path}' alt='${card.name}' />
-        <div class='actor-name'>${card.name}</div>
-        <button class='subscribe-btn'>Подписаться</button>
-    `;
+    function getList() {
+        return Lampa.Storage.get(STORAGE_KEY, []);
+    }
 
-    var btn = cardEl.querySelector('.subscribe-btn');
-    btn.addEventListener('click', function() {
-        if (btn.classList.contains('subscribed')) {
-            btn.classList.remove('subscribed');
-            btn.textContent = 'Подписаться';
-        } else {
-            btn.classList.add('subscribed');
-            btn.textContent = 'Отписаться';
+    function toggle(id) {
+        var list = getList();
+        var index = list.indexOf(id);
+
+        if (index === -1) list.push(id);
+        else list.splice(index, 1);
+
+        Lampa.Storage.set(STORAGE_KEY, list);
+        return index === -1;
+    }
+
+    function isSaved(id) {
+        return getList().includes(id);
+    }
+
+    function detectId(activity) {
+        if (!activity) return null;
+
+        if (activity.id) return parseInt(activity.id, 10);
+        if (activity.object && activity.object.id) return parseInt(activity.object.id, 10);
+        if (activity.params && activity.params.id) return parseInt(activity.params.id, 10);
+
+        var match = location.pathname.match(/(person|actor)\/(\d+)/);
+        if (match) return parseInt(match[2], 10);
+
+        return null;
+    }
+
+    function waitContainer(cb) {
+        var tries = 0;
+        (function check() {
+            var el = document.querySelector('.person-start__bottom');
+            if (el) cb(el);
+            else if (tries++ < 30) setTimeout(check, 200);
+        })();
+    }
+
+    function addButton(container) {
+        if (!currentId) return;
+
+        var old = container.querySelector('.persons-btn');
+        if (old) old.remove();
+
+        var btn = document.createElement('div');
+        btn.className = 'full-start__button selector persons-btn';
+
+        function render() {
+            btn.innerHTML = '<span>' + (isSaved(currentId) ? 'Отписаться' : 'Подписаться') + '</span>';
         }
-    });
 
-    container.appendChild(cardEl);
-},
+        render();
 
-// Инициализация списка актёров
-initActors: function(actorIds) {
-    actorIds.forEach(function(id) {
-        ActorModule.getActor(id, function(card) {
-            ActorModule.createCard(card);
-            console.log('CARD FIXED:', card);
+        btn.addEventListener('hover:enter', function () {
+            toggle(currentId);
+            render();
         });
-    });
-}
 
-};
+        container.appendChild(btn);
 
-// Пример использования: ActorModule.initActors([976, 74251, 232192, 62220, 2288100]);
+        log('BUTTON:', currentId);
+    }
+
+    function PersonsSource() {
+        var cache = {};
+
+        this.list = function (params, done) {
+            var page = parseInt(params.page || 1, 10);
+            var ids = getList();
+
+            var slice = ids.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE);
+
+            if (!slice.length) {
+                done({ results: [] });
+                return;
+            }
+
+            var loaded = 0;
+            var results = [];
+
+            slice.forEach(function (id) {
+
+                if (cache[id]) {
+                    results.push(cache[id]);
+                    return check();
+                }
+
+                var url = Lampa.TMDB.api(
+                    'person/' + id +
+                    '?api_key=' + Lampa.TMDB.key() +
+                    '&language=ru'
+                );
+
+                new Lampa.Reguest().silent(url, function (r) {
+
+                    var j = typeof r === 'string' ? JSON.parse(r) : r;
+
+                    var card = {
+                        id: parseInt(j.id, 10),
+
+                        title: j.name,
+                        name: j.name,
+
+                        poster_path: j.profile_path,
+                        profile_path: j.profile_path,
+
+                        type: 'person',
+                        card_type: 'person',
+
+                        component: 'person',   // 🔥 ГЛАВНЫЙ ФИКС
+                        method: 'person',
+
+                        source: 'tmdb',
+                        media_type: 'person',
+
+                        url: 'person/' + j.id  // 🔥 ГЛАВНЫЙ ФИКС
+                    };
+
+                    log('CARD OK:', card);
+
+                    cache[id] = card;
+                    results.push(card);
+
+                    check();
+
+                }, function () {
+                    check();
+                });
+            });
+
+            function check() {
+                loaded++;
+                if (loaded >= slice.length) {
+                    done({
+                        results: results,
+                        page: page,
+                        total_pages: Math.ceil(ids.length / PAGE_SIZE)
+                    });
+                }
+            }
+        };
+
+        // 🔥 фикс ошибки full()
+        this.full = function (params, onComplete, onError) {
+            if (Lampa.Api.sources.tmdb && Lampa.Api.sources.tmdb.full) {
+                Lampa.Api.sources.tmdb.full(params, onComplete, onError);
+            } else {
+                onError && onError();
+            }
+        };
+    }
+
+    function start() {
+
+        if (!Lampa.Storage.get(STORAGE_KEY)) {
+            Lampa.Storage.set(STORAGE_KEY, []);
+        }
+
+        Lampa.Api.sources[PLUGIN_NAME] = new PersonsSource();
+
+        // меню
+        var item = $('<li class="menu__item selector"><div class="menu__text">Персоны</div></li>');
+
+        item.on('hover:enter', function () {
+            Lampa.Activity.push({
+                component: 'category_full',
+                source: PLUGIN_NAME,
+                title: 'Персоны'
+            });
+        });
+
+        $('.menu .menu__list').eq(0).append(item);
+
+        // отслеживание страницы актёра
+        Lampa.Listener.follow('activity', function (e) {
+
+            if (e.type === 'start' && (e.component === 'person' || e.component === 'actor')) {
+
+                currentId = detectId(e);
+
+                log('OPEN:', currentId);
+
+                if (currentId) {
+                    waitContainer(addButton);
+                }
+            }
+        });
+
+        // если уже открыта
+        setTimeout(function () {
+            var act = Lampa.Activity.active();
+
+            if (act && (act.component === 'person' || act.component === 'actor')) {
+                currentId = detectId(act);
+
+                if (currentId) {
+                    waitContainer(addButton);
+                }
+            }
+        }, 1000);
+    }
+
+    if (window.appready) start();
+    else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type === 'ready') start();
+        });
+    }
+
+})();
